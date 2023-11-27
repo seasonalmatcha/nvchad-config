dcff064e5b4c7996dddd919d5aec6860af24e7ff
Alan H <me@alanh.dev>
2023-11-27 17:03:00 +0700

    feat: add formatter for js and ts react

--- lua/custom/configs/formatter.lua
+++ lua/custom/configs/formatter.lua
@@ -3,8 +3,14 @@ local M = {
     javascript = {
       require("formatter.filetypes.javascript").prettier
     },
+    javascriptreact = {
+      require("formatter.filetypes.javascriptreact").prettier
+    },
     typescript = {
       require("formatter.filetypes.typescript").prettier
+    },
+    typescriptreact = {
+      require("formatter.filetypes.typescriptreact").prettier
     },
     go = {
       require("formatter.filetypes.go").gofumpt,
