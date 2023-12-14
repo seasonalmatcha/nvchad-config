74e56c5f3da430b1f82fe604a25a50121721f5a7
Alan H <me@alanh.dev>
2023-12-14 23:19:55 +0700

    feat: add more events for linter

--- lua/custom/configs/lint.lua
+++ lua/custom/configs/lint.lua
@@ -1,10 +1,15 @@
 require("lint").linters_by_ft = {
-  javascript = { "eslint" },
-  typescript = { "eslint" },
+  javascript = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+  typescript = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
   go = { "golangcilint" },
 }
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePre", "InsertLeave", "TextChanged" }, {
+  group = lint_augroup,
   callback = function()
     require("lint").try_lint()
   end,
